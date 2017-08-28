955f61220973e667726241a80ef86d3e353e5e34
Xie Yiyu <yyxie@bainainfo.com>
2017-08-28 11:05:06 +0800

    修改首次进入应用用例，添加实体按键操作

--- pageobejct/base_operate.py
+++ pageobejct/base_operate.py
@@ -15,16 +15,16 @@ class BaseOperate:
 
     def find_element(self, case_operate):
         """
-        查找元素是否存在
+        重写元素定位方法
         :param case_operate: 用例操作
         :return:
         """
         try:
-            if type(case_operate) == list: # 多检查点
+            if type(case_operate) == list: # 多个元素
                 for item in case_operate:
                     WebDriverWait(self.driver, BaseElement.WAIT_TIME).until(lambda x: self.find_element_by(item))
                 return True
-            if type(case_operate) == dict: # 单检查点
+            if type(case_operate) == dict: # 单个元素
                 WebDriverWait(self.driver, BaseElement.WAIT_TIME).until(lambda x: self.find_element_by(case_operate))
                 return True
         except selenium.common.exceptions.TimeoutException:
@@ -32,8 +32,7 @@ class BaseOperate:
         except selenium.common.exceptions.NoSuchElementException:
             return False
 
-    # 封装操作
-    def operate(self, case_operate, test_info): # 未写入log
+    def operate(self, case_operate, test_info):
         """
         封装元素操作方法
         :param case_operate: 用例操作
@@ -45,12 +44,28 @@ class BaseOperate:
         if self.find_element(case_operate):
             elements = {
                 BaseElement.CLICK: lambda: self.click(case_operate),
+                BaseElement.SET_VALUE: lambda : self.set_value(case_operate),
                 BaseElement.SWIPE_LEFT: lambda: self.swipe_left(case_operate),
-                BaseElement.SET_VALUE: lambda : self.set_value(case_operate)
+                BaseElement.SWIPE_RIGHT: lambda: self.swipe_right(case_operate),
+                BaseElement.SWIPE_UP: lambda: self.swipe_up(case_operate),
+                BaseElement.SWIPE_DOWN: lambda: self.swipe_down(case_operate),
+                BaseElement.KEY_EVENT: lambda : self.key_event(case_operate)
             }
             elements[case_operate['operate_type']]()
             return True
         return False
+
+    def find_element_by(self, case_operate):
+        """
+        封装常用元素定位方法
+        """
+        elements = {
+            BaseElement.find_element_by_id: lambda: self.driver.find_element_by_id(case_operate['element_info']),
+            BaseElement.find_element_by_name: lambda: self.driver.find_element_by_name(case_operate['element_info']),
+            BaseElement.find_element_by_class_name: lambda: self.driver.find_element_by_class_name(case_operate['element_info']),
+            BaseElement.find_element_by_xpath: lambda: self.driver.find_element_by_xpath(case_operate['element_info'])
+        }
+        return elements[case_operate['find_type']]()
 
     def click(self, case_operate):
         """
@@ -66,37 +81,61 @@ class BaseOperate:
         self.find_element_by(case_operate).set_value(case_operate["text"])
         log.info("set %s" % case_operate["text"])
 
-    # 左滑动
-    def swipe_left(self, case_operate):
+    def get_window_size(self):
         """
-        左滑动
-        """
-        width = self.driver.get_window_size()['width'] # 待改进：获取window_size应封装
-        height = self.driver.get_window_size()['height']
-        for i in range(case_operate['times']):
-            self.driver.swipe(width * 0.8, height * 0.5, width * 0.2, height * 0.5, case_operate['swipe_time'])
-            log.info("The %s swipe_left" % i)
-            sleep(1)
-
-    def swipe_right(self, case_operate):
-        """
-        右滑动
+        获取屏幕分辨率
+        :return: width*height
         """
         width = self.driver.get_window_size()['width']
         height = self.driver.get_window_size()['height']
-        for i in range(case_operate['times']):
-            self.driver.swipe(width * 0.2, height * 0.5, width * 0.8, height * 0.5, case_operate['swipe_time'])
-            log.info("The %s swipe_right" % i)
+        return width,height
+
+    def swipe_to(self, x1, y1, x2, y2, times, swipe_time):
+        """
+        滑动屏幕
+        :param x1: 起点横坐标
+        :param y1: 起点纵坐标
+        :param x2: 重点横坐标
+        :param y2: 终点纵坐标
+        :param times: 滑动次数
+        :param swipe_time: 滑动时间
+        :return:
+        """
+        width, height = self.get_window_size()
+        for i in range(times):
+            self.driver.swipe(width * x1, height * y1, width * x2, height * y2, swipe_time)
             sleep(1)
 
-    def find_element_by(self, case_operate):
+    def swipe_left(self, case_operate):
         """
-        封装常用元素定位方法
+        左滑
         """
-        elements = {
-            BaseElement.find_element_by_id: lambda: self.driver.find_element_by_id(case_operate['element_info']),
-            BaseElement.find_element_by_name: lambda: self.driver.find_element_by_name(case_operate['element_info']),
-            BaseElement.find_element_by_class_name: lambda: self.driver.find_element_by_class_name(case_operate['element_info']),
-            BaseElement.find_element_by_xpath: lambda: self.driver.find_element_by_xpath(case_operate['element_info'])
-        }
-        return elements[case_operate['find_type']]()
+        self.swipe_to(0.8, 0.5, 0.2, 0.5, case_operate['times'], case_operate['swipe_time'])
+
+    def swipe_right(self, case_operate):
+        """
+        右滑
+        """
+        self.swipe_to(0.2, 0.5, 0.8, 0.5, case_operate['times'], case_operate['swipe_time'])
+
+    def swipe_up(self, case_operate):
+        """
+        上滑
+        """
+        self.swipe_to(0.5, 0.8, 0.5, 0.2, case_operate['times'], case_operate['swipe_time'])
+
+    def swipe_down(self, case_operate):
+        """
+        下滑
+        """
+        self.swipe_to(0.5, 0.2, 0.5, 0.8, case_operate['times'], case_operate['swipe_time'])
+
+    def key_event(self, case_operate):
+        """
+        操作实体按键
+        按键码参考链接http://blog.csdn.net/qq_22795513/article/details/53169593
+        :return:
+        """
+        log.info("key_event: " % case_operate)
+        self.driver.keyevent(case_operate['key_code'])
+
